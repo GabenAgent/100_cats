332c74dfa3298b97b6a0c7cbf98ef6da35566c81
GabenAgent <dudkawork@gmail.com>
2024-05-07 10:54:41 +0300

    Assign 12

--- lessons/assignment 12.py
+++ lessons/assignment 12.py
@@ -1,18 +1,24 @@
 def is_admin(func):
     def wrapper(*args, **kwargs):
-        return wrapper()
+        user_type = kwargs.get("user_type")
+        if user_type == "admin":
+            return func(*args, **kwargs)
+        else:
+            raise ValueError("Permission denied")
+
+    return wrapper
 
 
 @is_admin
 def show_customer_receipt(user_type: str):
-    # Some very dangerous operation
+    print("Success")
+
 
 try:
     show_customer_receipt(user_type='user')
-    raise ValueError("Permission denied")
+except ValueError as e:
+    assert str(e) == "Permission denied"
+
 
 show_customer_receipt(user_type='admin')
-
-
-
-
+show_customer_receipt(user_type='user')
