036dcd2b6981982d9ee65d3ff11984b8b2f4ac6e
GabenAgent <dudkawork@gmail.com>
2024-05-08 01:50:29 +0300

    Assign 12 complete

--- lessons/assignment 12.py
+++ lessons/assignment 12.py
@@ -22,3 +22,22 @@ except ValueError as e:
 
 show_customer_receipt(user_type='admin')
 show_customer_receipt(user_type='user')
+
+
+def catch_errors(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except KeyError:
+            print("Found 1 error during execution of your function: KeyError no such key as foo")
+
+    return wrapper
+
+
+@catch_errors
+def some_function_with_risky_operation(data):
+    print(data['key'])
+
+
+some_function_with_risky_operation({'foo': 'bar'})
+some_function_with_risky_operation({'key': 'bar'})
